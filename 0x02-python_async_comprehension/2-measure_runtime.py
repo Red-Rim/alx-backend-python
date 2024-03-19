c24a722144d1009400c29d0b9d69ef8b232abcb3
Red-Rim <salmaouakhir20@gmail.com>
2024-03-19 16:55:25 +0000

    runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,11 +2,9 @@
 """ Async Comprehensions
 """
 
-
 import time
 import asyncio
-from typing import Coroutine
-
+from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -14,5 +12,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the total runtime"""
     start_time: float = time.time()
-    await asyncio.gather(*(async_comprehension() for i in range(4)))  # type: Coroutine
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     return time.time() - start_time
+
+
+if __name__ == "__main__":
+    print(asyncio.run(measure_runtime()))
